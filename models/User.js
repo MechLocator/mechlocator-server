e6ab863cc771f63e575e3fc634baab4d755fafa0
MechLocator <mechlocator@gmail.com>
2023-09-05 12:52:22 -0700

    changed User model's identation

--- models/User.js
+++ models/User.js
@@ -1,51 +1,52 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        // match: /.+\@.+\..+/,
-        unique: true,
-    },
-    image: {
-        type: String,
-    },
-    password: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    accountType: {
-        type: String,
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    isEditor: {
-        type: Boolean,
-        default: false,
-    },
-    // isPartner: {
-    //     type: Boolean,
-    //     default: false,
-    // },
-    // isDriver: {
-    //     type: Boolean,
-    //     default: false,
-    // },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    isSuspended: {
-        type: Boolean,
-        default: false,
-    },
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            // match: /.+\@.+\..+/,
+            unique: true,
+        },
+        image: {
+            type: String,
+        },
+        password: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        accountType: {
+            type: String,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        isEditor: {
+            type: Boolean,
+            default: false,
+        },
+        // isPartner: {
+        //     type: Boolean,
+        //     default: false,
+        // },
+        // isDriver: {
+        //     type: Boolean,
+        //     default: false,
+        // },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        isSuspended: {
+            type: Boolean,
+            default: false,
+        },
     },
     { timestamps: true }
 )

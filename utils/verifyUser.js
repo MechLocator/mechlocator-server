d92a3ab9a39326a935278add5394be7258179f96
MechLocator <mechlocator@gmail.com>
2023-09-21 12:56:26 -0700

    Removed the requirement for admin or editor rights when fetching a user by their uid

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -2,7 +2,7 @@ import verifyToken from "./tokenVerifier.js"
 
 export default function verifyUser (req, res, next) {
     verifyToken(req, res, next, () => {
-      if (req.user.id === req.params.id || req.user.isAdmin || req.user.isEditor) {
+      if (req.user.id === req.params.id) {
         next();
       } else {
         return next(createError(403, 'You are not an authorized user!'));

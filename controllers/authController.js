d146d5ceb5fd2bb963b61319f93bd29044eea0ea
MechLocator <mechlocator@gmail.com>
2023-10-05 14:14:29 -0700

    Added a logout route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
-import User from '../models/User.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import { createError } from '../utils/error.js';
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -23,7 +23,7 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if (!user) return next(createError(404, 'Sorry, user not found!'));
+    if (!user) return next(createError(404, "Sorry, user not found!"));
 
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
@@ -31,60 +31,75 @@ export const login = async (req, res, next) => {
     );
 
     if (!isPasswordCorrect)
-      return next(createError(400, 'Wrong username or password'));
+      return next(createError(400, "Wrong username or password"));
 
     const token = jwt.sign(
-      { 
-        id: user._id, 
-        isAdmin: user.isAdmin, 
-        isEditor: user.isEditor, 
-        isVerified: user.isVerified, 
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+        isEditor: user.isEditor,
+        isVerified: user.isVerified,
         isSuspended: user.isSuspended,
-        isPartner: user.isPartner
+        isPartner: user.isPartner,
       },
       process.env.JWT_SECRET_KEY
     );
 
-    const { password, isAdmin, isEditor, isVerified, isPartner, isSuspended, ...otherDetails } = user._doc;
+    const {
+      password,
+      isAdmin,
+      isEditor,
+      isVerified,
+      isPartner,
+      isSuspended,
+      ...otherDetails
+    } = user._doc;
     res
-      .cookie('access_token', token, {
+      .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json({ details: { ...otherDetails }, isAdmin, isVerified, isSuspended, isPartner, token });
+      .json({
+        details: { ...otherDetails },
+        isAdmin,
+        isVerified,
+        isSuspended,
+        isPartner,
+        token,
+      });
   } catch (err) {
     next(err);
   }
 };
 
-export const resetPassword = async (req,_,next) => {
+export const resetPassword = async (req, _, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if (!user) return next(createError(404, 'Sorry, user not found!'));
+    if (!user) return next(createError(404, "Sorry, user not found!"));
 
     // Generate a random set of numbers, maximum length of 6 which we'll send to a user's email
     function generateRandomNumber(length) {
       const numbers = "0123456789";
       let result;
-      const numLength = numbers.length
-      for( let i = 0; i < length; i++) {
-        result += numbers.charAt(Math.floor(Math.random() * numLength))
+      const numLength = numbers.length;
+      for (let i = 0; i < length; i++) {
+        result += numbers.charAt(Math.floor(Math.random() * numLength));
       }
-      return result
+      return result;
     }
 
-    const codeToSend = generateRandomNumber(6)
+    const codeToSend = generateRandomNumber(6);
 
     // Assign a password reset token for this user
     const token = jwt.sign(
-      {id: user._id, code: codeToSend},
+      { id: user._id, code: codeToSend },
       process.env.JWT_PASSWORD_RESET_KEY
-    )
+    );
 
     // Send this token somehow to the user's email and have them confirm it in the app
-    console.log(JSON.stringify(token))
+    console.log(JSON.stringify(token));
   } catch (error) {
-    next(error)
+    next(error);
   }
 
   // If the email exists in the MongoDB database, send a reset code to that email
@@ -93,24 +108,42 @@ export const resetPassword = async (req,_,next) => {
   // If the codes match, we proceed to the next stage
   // which is inputing a new password
   // This new password is then saved and then the user is prompted to login once again
-}
+};
 
-export const isAuth = async (req,res,next) => {
+export const isAuth = async (req, res, next) => {
   const authHeader = req.get("Authorization");
-    if (!authHeader) {
-        return res.status(401).json({ message: 'not authenticated' });
-    };
-    const token = authHeader.split(' ')[1];
-    let decodedToken; 
-    try {
-        decodedToken = jwt.verify(token, 'secret');
-    } catch (err) {
-        next(err)
-        return res.status(500).json({ message: err.message || 'could not decode the token' });
-    };
-    if (!decodedToken) {
-        res.status(401).json({ message: 'unauthorized' });
+  if (!authHeader) {
+    return res.status(401).json({ message: "not authenticated" });
+  }
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, "secret");
+  } catch (err) {
+    next(err);
+    return res
+      .status(500)
+      .json({ message: err.message || "could not decode the token" });
+  }
+  if (!decodedToken) {
+    res.status(401).json({ message: "unauthorized" });
+  } else {
+    res.status(200).json({ message: "You are authenticated!!" });
+  }
+};
+
+export const signOut = (req, res) => {
+  if (req.headers && req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authorization Has Failed!",
+      });
     } else {
-        res.status(200).json({ message: 'here is your resource' });
-    };
-}
+      req.body.token = null;
+      req.session.loggedIn = false;
+      res.clearCookie("access_token");
+    }
+  }
+};

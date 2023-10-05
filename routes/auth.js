891275394bf4ca809173df2749a3748ca9894455
MechLocator <mechlocator@gmail.com>
2023-10-05 14:20:13 -0700

    Added a logout route the /routes/auth.js directory

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,14 @@ import {
   login,
   register,
   resetPassword,
+  signOut,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/sign-out", isAuth, signOut);
 router.post("/reset-password", resetPassword);
 router.get("/private", isAuth);
 export default router;

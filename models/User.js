ff938e4dff5e8b6791551cb01d72f99a41dd6aa1
MechLocator <mechlocator@gmail.com>
2023-10-14 17:49:15 -0700

    modified: models/User.js

--- models/User.js
+++ models/User.js
@@ -38,6 +38,9 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    location: {
+      type: String,
+    },
     owner_uid: {
       type: String,
     },

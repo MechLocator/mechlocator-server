98ab7d98c56705789995e3d03c9cca1d469dbbef
MechLocator <mechlocator@gmail.com>
2023-11-08 15:39:05 -0800

    modified: controllers/authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -163,13 +163,11 @@ export const updatePassword = async (req, res, next) => {
       { $set: { ...req.body, password: hash } },
       { new: true }
     );
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "User password updated successfully",
-        updatedUser,
-      });
+    res.status(200).json({
+      success: true,
+      message: "User password updated successfully",
+      updatedUser,
+    });
   } catch (err) {
     next(err);
   }

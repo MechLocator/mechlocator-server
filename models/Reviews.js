83a406601cafa1b449d32e83e7a1df387e062f3e
MechLocator <mechlocator@gmail.com>
2023-11-09 11:04:04 -0800

    modified: models/Reviews.js

--- models/Reviews.js
+++ models/Reviews.js
@@ -14,6 +14,10 @@ const ReviewsSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    wordRating: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );

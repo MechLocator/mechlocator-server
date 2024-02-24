152c81dc6f46020d84b38364ff38ff2cee8ac7e2
MechLocator <mechlocator@gmail.com>
2024-02-24 11:38:03 -0800

    added a server signOut controller function

--- routes/dashAuth.js
+++ routes/dashAuth.js
@@ -5,24 +5,11 @@ import {
   register,
   resetPassword,
   sendPassCodeToEmail,
+  signOut,
 } from "../controllers/dashAuthController.js";
 import { userAuth } from "../utils/jsontokenVerifier.js";
-import AuthUser from "../models/AuthUser.js";
 
 const router = express.Router();
-
-/**
- * @DESC Check Role Middleware
- */
-// const checkRole = roles => async (req, res, next) => {
-//   let { name } = req.body;
-
-//retrieve employee info from DB
-//   const user = await AuthUser.findOne({ name });
-//   !roles.includes(user?.role)
-//     ? res.status(401).json("Sorry you do not have access to this route")
-//     : next();
-// };
 
 router.post("/admin-register", async (req, res) => {
   await register(req.body, "admin", res);
@@ -36,20 +23,17 @@ router.post("/admin-login", async (req, res) => {
 router.post("/editor-login", async (req, res) => {
   await login(req.body, "editor", res);
 });
-router.post("/reset-password", resetPassword);
-router.post("/create-dash-user", createDashUser);
-router.post("/pass-to-email", sendPassCodeToEmail); // only admins or editors can access this route
-router.get(
-  "/return-something",
-  userAuth(["admin"]),
-  //   checkRole(["admin"]),
-  async (req, res) => {
-    // console.log(req);
-    return res
-      .status(202)
-      .json(`welcome user. You're allowed to view this route as an admin`);
-  }
-);
+// Implement sign out route
+router.get("/sign-out", userAuth(["admin", "editor"]), signOut);
+router.post("/reset-password", userAuth(["admin", "editor"]), resetPassword);
+router.post("/create-dash-user", userAuth(["admin"]), createDashUser);
+router.post("/pass-to-email", userAuth(["admin"]), sendPassCodeToEmail); // only admins or editors can access this route
+router.get("/return-something", userAuth(["admin"]), async (req, res) => {
+  // console.log(req);
+  return res
+    .status(202)
+    .json(`welcome user. You're allowed to view this route as an admin`);
+});
 router.get(
   "/return-something-else",
   userAuth(["editor"]),

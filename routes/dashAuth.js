f8cb0a5fb486f6eddf2ff2691bc3ff28e9e84521
MechLocator <mechlocator@gmail.com>
2024-03-14 16:07:43 -0700

    added authorised access to the updateUser controller function which is ran upon navigating to the update-user route

--- routes/dashAuth.js
+++ routes/dashAuth.js
@@ -38,7 +38,7 @@ router.get("/sign-out", isAuth, userAuth(["admin", "editor"]), signOut);
  includes verification and suspension.
 */
 
-router.put("/update-user/:id", updateUser);
+router.put("/update-user/:id", userAuth(["admin", "editor"]), updateUser);
 // create a dashboard users route
 router.post("/reset-password", userAuth(["admin", "editor"]), resetPassword); // both admins and editors can reset their own passwords
 router.post("/create-dash-user", userAuth(["admin"]), createDashUser);
